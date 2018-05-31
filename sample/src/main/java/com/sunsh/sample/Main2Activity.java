985802db4631539c1c3e1ff1f6d6fd9b34ee91e6
sunshaobei <sunshaobei@foxmail.com>
2018-05-31 15:08:20 +0800

    change

--- sample/src/main/java/com/sunsh/sample/Main2Activity.java
+++ sample/src/main/java/com/sunsh/sample/Main2Activity.java
@@ -1,8 +1,10 @@
-package com.sunsh.subscibeevent;
+package com.sunsh.sample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+
+import com.sunsh.subscibeevent.SubscribeEvent;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -10,10 +12,10 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-
     }
 
     public void event(View view) {
-        SubscribeEvent.getInstance().exposeEvent("event","text",333);
+        SubscribeEvent.getInstance().exposeEvent("event","text",555,false);
+        finish();
     }
 }
--- sample/src/main/java/com/sunsh/sample/MainActivity.java
+++ sample/src/main/java/com/sunsh/sample/MainActivity.java
@@ -1,9 +1,12 @@
-package com.sunsh.subscibeevent;
+package com.sunsh.sample;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+
+import com.sunsh.subscibeevent.SubscribeEvent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,13 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SubscribeEvent.getInstance().subscribe("event",this);
         startActivity(new Intent(this,Main2Activity.class));
-        SubscribeEvent.getInstance().subscribe("event", this);
     }
 
-    private void event(String str, Integer i) {
+    private void event(String s,Integer i,Boolean b){
         TextView viewById = findViewById(R.id.textview);
-        viewById.setText(str + i);
+         s = s+i+b+".........................";
+        viewById.setText(s+s+s);
+        viewById.setSelected(true);
     }
 
     @Override
